aa60469dd85cfca5e575ea58b7053d4ae1752cf1
Ahmad Umar <ahmad.umar@bibit.id>
2023-05-10 18:23:16 +0700

    log payload injson

--- src/middlewares/http-request-logger.middleware.ts
+++ src/middlewares/http-request-logger.middleware.ts
@@ -1,10 +1,19 @@
 import morgan from 'morgan';
 import { get, isEmpty } from 'lodash';
+import { createWriteStream } from "fs"
+import path = require("path")
 
 const httpRequestLoggerMiddleware = morgan((tokens, req, res) => {
   const body = get(req, 'body');
   if (!isEmpty(body)) {
     console.log(`${get(req, 'requestId')} |`, JSON.stringify(body));
+    const unixTimeStamp = Math.floor(new Date().getTime() / 1000)
+    const outputDirectory = path.join(__dirname, '../../log', `${unixTimeStamp}.json`);
+
+    const fileDir = createWriteStream(outputDirectory)
+
+    fileDir.write(JSON.stringify(JSON.stringify(body)))
+    console.info(`log file..`, outputDirectory);
   }
   return [
     get(req, 'requestId'),
